f5810b1e1d71f2454d93a550d9c92cdf288993a2
Abhay1509 <chaudharyyyabhay@gmail.com>
2023-06-15 15:23:58 +0530

    second commit

--- src/components/LoginButton.jsx
+++ src/components/LoginButton.jsx
@@ -2,13 +2,11 @@ import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const LoginButton = () => {
-    const {loginWithRedirect} = useAuth0();
-    return(
-        <div>
-            <button onClick={()=> loginWithRedirect()}>
-                Login
-            </button>
-        </div>
-    )
-}
+  const { loginWithRedirect } = useAuth0();
+  return (
+    <>
+      <button onClick={() => loginWithRedirect()}>Log In</button>
+    </>
+  );
+};
 export default LoginButton;
